5fc0e5476ff5a4678274bca12b362c8f17bcbe1a
dair21327-cpu <dair21327@gmail.com>
2026-03-15 23:47:47 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import random
 
+boxes = list(range(1, 101))
+random.shuffle(boxes)
+
+all_alive = True
+
+for prisoner in range(1, 101):
+
+    found = False
+
+    for box in range(50):
+        if boxes[box] == prisoner:
+            print(f"Заключенный {prisoner} нашел свой номер в коробке {box+1}")
+            found = True
+            break
+
+    if not found:
+        print(f"Заключенный {prisoner} не нашел свой номер. Все проиграли.")
+        all_alive = False
+        break
+
+if all_alive:
+    print("Все заключенные выжили!")
+else:
+    print("Все заключенные погибли.")
